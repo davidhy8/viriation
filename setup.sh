d46849cb513ad120bfbef499e07f97986a0c547a
davidhy8 <david.yang1@ucalgary.ca>
2024-09-19 09:13:17 -0600

    update setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+BASE_PATH=/home/david.yang1/autolit/viriation/
+cd $BASE_PATH
+
+# Set up directories
+mkdir data 
+cd data
+mkdir scraper
+cd scraper
+mkdir pubmed rxiv
+cd ../pipeline_data
+mkdir NER
